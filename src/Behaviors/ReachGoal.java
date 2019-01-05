b1621f254104c50e0e7a9011db22dfd996e4dcd5
Adamantios <manoszaras94@gmail.com>
2019-01-05 20:41:11 +0200

    Decrease stop point and follow the line until the end, in order to reach the goal.

--- src/Behaviors/ReachGoal.java
+++ src/Behaviors/ReachGoal.java
@@ -5,14 +5,23 @@ import Utilities.SensorsInterpreter;
 import Utilities.Velocities;
 
 public class ReachGoal extends Behavior {
-    private static final double LUMINANCE_STOP_POINT = 0.93;
+    private static final double LUMINANCE_STOP_POINT = 0.85;
 
     public ReachGoal(Sensors sensors) {
         super(sensors);
     }
 
     public Velocities act() {
-        return new Velocities(0, 0);
+        int numOfSensors = getSensors().getLine().getNumSensors();
+        SensorsInterpreter.LineSensorHalfs lineSensorHalfs =
+                new SensorsInterpreter.LineSensorHalfs(getSensors().getLine());
+        int right = lineSensorHalfs.getRight();
+        int left = lineSensorHalfs.getLeft();
+
+        if (left == 0 && right == 0)
+            return new Velocities(0, 0);
+        else
+            return new Velocities(TRANSLATIONAL_VELOCITY, (double) (left - right) / numOfSensors * 5);
     }
 
     public boolean isActive() {
