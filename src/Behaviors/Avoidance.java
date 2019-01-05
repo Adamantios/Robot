40dc10cff4ea322df28515835690d7424071c72e
Adamantios <manoszaras94@gmail.com>
2019-01-05 19:20:20 +0200

    Remove unused parameters and activate when the hit sonar has the minimum distance or less, not always, because sonar's range is 1.5 meters.

--- src/Behaviors/Avoidance.java
+++ src/Behaviors/Avoidance.java
@@ -13,14 +13,10 @@ public class Avoidance extends Behavior {
     private static final double K1 = 5;
     private static final double K2 = 0.8;
     private static final double K3 = 1;
-    private static final double SAFETY_DISTANCE = 0.8;
+    private static final double START_DISTANCE = 0.4;
+    private static final double SAFETY_DISTANCE = 0.6;
 
-    private boolean completed;
-    private double fToGoal;
-    private double initialDist;
-    private boolean circleCompleted;
     private float robotRadius;
-    private Point3d hitPoint;
 
     public Avoidance(Sensors sensors, float robotRadius) {
         super(sensors);
@@ -30,25 +26,35 @@ public class Avoidance extends Behavior {
     public Velocities act() {
         RangeSensorBelt sonars = getSensors().getSonars();
 
-        int min;
-        min = 0;
+        int min = 0;
         for (int i = 1; i < sonars.getNumSensors(); i++)
             if (sonars.getMeasurement(i) < sonars.getMeasurement(min))
                 min = i;
+
         Point3d p = SensorsInterpreter.getSensedPoint(robotRadius, sonars, min);
         double d = p.distance(new Point3d(0, 0, 0));
         Vector3d v;
         v = CLOCKWISE ? new Vector3d(-p.z, 0, p.x) : new Vector3d(p.z, 0, -p.x);
         double phLin = Math.atan2(v.z, v.x);
         double phRot = Math.atan(K3 * (d - SAFETY_DISTANCE));
+
         if (CLOCKWISE)
             phRot = -phRot;
+
         double phRef = SensorsInterpreter.wrapToPi(phLin + phRot);
 
         return new Velocities(K2 * Math.cos(phRef), K1 * phRef);
     }
 
     public boolean isActive() {
-        return getSensors().getBumpers().oneHasHit() || getSensors().getSonars().oneHasHit();
+        RangeSensorBelt sonars = getSensors().getSonars();
+
+        int min = 0;
+        for (int i = 1; i < sonars.getNumSensors(); i++)
+            if (sonars.getMeasurement(i) < sonars.getMeasurement(min))
+                min = i;
+
+        return getSensors().getBumpers().oneHasHit() || getSensors().getSonars().getMeasurement(min) <= START_DISTANCE;
+
     }
 }
