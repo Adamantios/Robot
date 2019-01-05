28b07f384033bca1e029073df575e70c248f6460
Adamantios <manoszaras94@gmail.com>
2019-01-05 19:17:04 +0200

    Change luminance stop point.

--- src/Behaviors/ReachGoal.java
+++ src/Behaviors/ReachGoal.java
@@ -5,7 +5,7 @@ import Utilities.SensorsInterpreter;
 import Utilities.Velocities;
 
 public class ReachGoal extends Behavior {
-    private static final double LUMINANCE_STOP_POINT = 0.87;
+    private static final double LUMINANCE_STOP_POINT = 0.93;
 
     public ReachGoal(Sensors sensors) {
         super(sensors);
