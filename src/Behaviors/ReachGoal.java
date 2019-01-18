e0e8de724d27aa448aa4922adde7714a0126f5a3
Adamantios <manoszaras94@gmail.com>
2019-01-18 17:18:00 +0200

    Reach goal when light is above a threshold.

--- src/Behaviors/ReachGoal.java
+++ src/Behaviors/ReachGoal.java
@@ -5,29 +5,22 @@ import Utilities.SensorsInterpreter;
 import Utilities.Velocities;
 
 public class ReachGoal extends Behavior {
-    private static final double LUMINANCE_STOP_POINT = 0.9;
+    private static final double LUMINANCE_STOP_POINT = 0.91;
 
     public ReachGoal(Sensors sensors) {
         super(sensors);
     }
 
     public Velocities act() {
-        int numOfSensors = getSensors().getLine().getNumSensors();
-        SensorsInterpreter.LineSensorHalfs lineSensorHalfs =
-                new SensorsInterpreter.LineSensorHalfs(getSensors().getLine());
-        int right = lineSensorHalfs.getRight();
-        int left = lineSensorHalfs.getLeft();
-
-        if (left == 0 && right == 0)
-            return new Velocities(0, 0);
-        else
-            return new Velocities(TRANSLATIONAL_VELOCITY, (double) (left - right) / numOfSensors * 5);
+        return new Velocities(0, 0);
     }
 
     public boolean isActive() {
         double lLux = getSensors().getLightL().getLux();
         double rLux = getSensors().getLightR().getLux();
         double currentLuminance = SensorsInterpreter.luxToLuminance(rLux, lLux);
+//TODO remove
+//        System.out.println(currentLuminance);
 
         return currentLuminance >= LUMINANCE_STOP_POINT;
     }
