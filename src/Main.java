48c2f89a6fb1f8bc4d72bfb8c5f479cfc8680d6f
Adamantios <manoszaras94@gmail.com>
2019-01-19 02:42:58 +0200

    Add new position for the robot.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,8 @@ public class Main {
         // Create robot.
 //        Robot robot = new Robot(new Vector3d(0.0D, 0.0D, 0.0D), "Manos Panos"); // center
 //        Robot robot = new Robot(new Vector3d(-7.5D, 0.0D, -5.5D), "Manos Panos"); // top
-        Robot robot = new Robot(new Vector3d(8.0D, 0.0D, 3.0D), "Manos Panos"); // down
+        Robot robot = new Robot(new Vector3d(-8D, 0.0D, -6D), "Manos Panos"); // top from line
+//        Robot robot = new Robot(new Vector3d(8.0D, 0.0D, 3.0D), "Manos Panos"); // down
 
         // Create environment.
         Environment environment = new Environment();
