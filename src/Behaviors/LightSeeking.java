ce2b5bfc13409014980e2ad06e66e53af25876db
Adamantios <manoszaras94@gmail.com>
2019-01-19 23:23:08 +0200

    Turn tolerance value for approximation to a static final parameter.

--- src/Behaviors/LightSeeking.java
+++ src/Behaviors/LightSeeking.java
@@ -5,6 +5,7 @@ import Utilities.SensorsInterpreter;
 import Utilities.Velocities;
 
 public class LightSeeking extends Behavior {
+    private final static float TOLERANCE_APPROXIMATION = .4F;
     private double minLuminance;
 
     public LightSeeking(Sensors sensors) {
@@ -16,17 +17,16 @@ public class LightSeeking extends Behavior {
      * Determine whether two numbers are approximately equal
      * by seeing if they are within a certain tolerance percentage.
      *
-     * @param desiredValue        the value to be checked for approximation.
-     * @param actualValue         the actual value.
-     * @param tolerancePercentage the tolerance value. -> 1 = 1%, 2.5 = 2.5%, etc.
+     * @param desiredValue the value to be checked for approximation.
+     * @param actualValue  the actual value.
      * @return true if the desiredValue is approximately equal with the actualValue, within the tolerancePercentage,
      * otherwise false.
      */
-    private static boolean approximatelyEqual(double desiredValue, double actualValue, double tolerancePercentage) {
+    private static boolean approximatelyEqual(double desiredValue, double actualValue) {
         // Get the absolute difference of the values.
         double diff = Math.abs(desiredValue - actualValue);
         // Calculate tolerance.
-        double tolerance = tolerancePercentage / 100 * desiredValue;
+        double tolerance = TOLERANCE_APPROXIMATION / 100 * desiredValue;
         // Return whether the absolute difference is smaller than the tolerance value or not.
         return diff < tolerance;
     }
@@ -47,7 +47,7 @@ public class LightSeeking extends Behavior {
         double translationalVelocity = 0;
 
         // Τurn towards light and move only if the right luminance is almost equal with the left.
-        if (approximatelyEqual(lLum, rLum, .4) || approximatelyEqual(rLum, lLum, .4)) {
+        if (approximatelyEqual(lLum, rLum) || approximatelyEqual(rLum, lLum)) {
             translationalVelocity = TRANSLATIONAL_VELOCITY;
 
             if (minLuminance > currentLuminance) {
