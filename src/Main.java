d64566d22193ebd61784ecd0b6f83d494bdeef54
Adamantios <manoszaras94@gmail.com>
2019-01-19 14:10:32 +0200

    Rename robot to Ma.Pa.

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // Create robot.
-//        Robot robot = new Robot(new Vector3d(0.0D, 0.0D, 0.0D), "Manos Panos"); // center
-//        Robot robot = new Robot(new Vector3d(-7.5D, 0.0D, -5.5D), "Manos Panos"); // top
-        Robot robot = new Robot(new Vector3d(-8D, 0.0D, -6D), "Manos Panos"); // top from line
-//        Robot robot = new Robot(new Vector3d(8.0D, 0.0D, 3.0D), "Manos Panos"); // down
+//        Robot robot = new Robot(new Vector3d(0.0D, 0.0D, 0.0D), "Ma.Pa."); // center
+//        Robot robot = new Robot(new Vector3d(-7.5D, 0.0D, -5.5D), "Ma.Pa."); // top
+        Robot robot = new Robot(new Vector3d(-8D, 0.0D, -6D), "Ma.Pa."); // top from line
+//        Robot robot = new Robot(new Vector3d(8.0D, 0.0D, 3.0D), "Ma.Pa."); // down
 
         // Create environment.
         Environment environment = new Environment();
