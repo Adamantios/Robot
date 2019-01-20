9ffdc4b5fdd1b0421ec772e8ab6049a77a7ceee1
Adamantios <manoszaras94@gmail.com>
2019-01-20 15:48:54 +0200

    Make tolerance more strict.

--- src/Robot.java
+++ src/Robot.java
@@ -115,8 +115,8 @@ class Robot extends Agent {
         double currentLuminance = SensorsInterpreter.luxToLuminance(sensors.getLightR().getLux(),
                 sensors.getLightL().getLux());
 
-        if (approximatelyEqual(currentLuminance, maxLuminanceDetected, .2F)
-                || approximatelyEqual(maxLuminanceDetected, currentLuminance, .2F))
+        if (approximatelyEqual(currentLuminance, maxLuminanceDetected, .005F)
+                || approximatelyEqual(maxLuminanceDetected, currentLuminance, .005F))
             checkCompleted = true;
         else
             this.setRotationalVelocity(Math.PI / 2);
